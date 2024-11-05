155cf6383c7d7c44b0136c5fec09c3480c7f25d5
unknown <paul9119298@gmail.com>
2024-11-05 23:06:28 +0900

    Revert "Update BoardController.java"
    This reverts commit 96110808ab95140bc751a76e5e4556b2059e1d83.

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -10,36 +10,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+
 import board.bean.BoardDTO;
 import board.service.BoardService;
 
 
 /* SpringReact/src/main/member/controller/BoardController.java */
 
-@Api(tags = "게시판 API") // BoardController에 대한 설명 추가
-@RestController
-@RequestMapping(value = "/board")
+//@Controller
+@RestController // @ResponseBody 없어도 바로 브라우저로 뿌려줌
+@RequestMapping(value="/board")
 @CrossOrigin(origins = "http://www.springreact.store", allowCredentials = "true")
 public class BoardController {
-
-    @Autowired
-    private BoardService boardService;
-
-    @ApiOperation(value = "게시글 작성", notes = "게시글의 제목과 내용을 입력받아 작성합니다.")
-    @GetMapping(value = "/boardWrite")
-    public void boardWrite(
-        @ApiParam(value = "게시글 제목", required = true) @RequestParam String subject,
-        @ApiParam(value = "게시글 내용", required = true) @RequestParam String content) {
-        System.out.println(subject);
-        boardService.boardWrite(subject, content);
-    }
-
-    @ApiOperation(value = "게시글 목록 조회", notes = "모든 게시글을 조회합니다.")
-    @GetMapping(value = "/boardList")
-    public List<BoardDTO> boardList() {
-        return boardService.boardList();
-    }
+	
+	@Autowired
+	private BoardService boardService;
+	
+//	@RequestMapping(value="/boardWrite", method = RequestMethod.POST)
+	@GetMapping(value="/boardWrite")
+//	@ResponseBody
+	public void boardWrite(@RequestParam String subject, @RequestParam String content) {
+		System.out.println(subject);
+		boardService.boardWrite(subject, content);
+	}
+	
+	
+	@GetMapping(value="/boardList")
+//	@ResponseBody
+	public List<BoardDTO> boardList() { // List<BoardDTO> 객체를 자동으로 JSON 배열로 변환하여 보냄
+		return boardService.boardList();
+		
+	}
+	
 }
