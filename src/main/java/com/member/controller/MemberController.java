0e86f9a93237176462e6fb27a5797a8c42b08f99
YoungSu Oh <110619825+YoungSuOh@users.noreply.github.com>
2024-11-05 22:29:41 +0900

    Update MemberController.java

--- src/main/java/com/member/controller/MemberController.java
+++ src/main/java/com/member/controller/MemberController.java
@@ -9,25 +9,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import member.service.MemberService;
 
 /* SpringReact/src/main/member/controller/MemberController.java */
+@Api(tags = "회원 API") // MemberController에 대한 설명 추가
 @Controller
 @RequestMapping(value = "/member")
 @CrossOrigin(origins = "http://www.springreact.store", allowCredentials = "true")
 public class MemberController {
-	@Autowired
-	MemberService memberService;
 
-	@PostMapping("/login")
-	@ResponseBody	
-	public String login(@RequestBody Map<String, String> credentials) {
-		String id = credentials.get("id");
-		String pwd = credentials.get("pwd");
-		return memberService.login(id, pwd);
-	}
-//    public String login(@RequestBody String id,@RequestBody String pwd) {
-//        return memberService.login(id,  pwd);
-//    }
+    @Autowired
+    MemberService memberService;
+
+    @ApiOperation(value = "로그인", notes = "회원의 아이디와 비밀번호를 입력받아 로그인합니다.")
+    @PostMapping("/login")
+    @ResponseBody
+    public String login(
+        @ApiParam(value = "회원 ID", required = true) @RequestBody Map<String, String> credentials) {
+        String id = credentials.get("id");
+        String pwd = credentials.get("pwd");
+        return memberService.login(id, pwd);
+    }
 }
